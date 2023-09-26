25a615ffaa70635b568d28355a2e3edc96a4e0c6
Midas <68521427+Muavia542@users.noreply.github.com>
2023-09-26 14:29:09 +0500

    Google pay Split Payment is also added
    The Google Pay Split Payment is added and the design is also updated

--- src/app/split-google-pay/recipient.interface.ts
+++
@@ -1,5 +0,0 @@
-// recipient.interface.ts
-export interface Recipient {
-  id: number;
-  percentage: number;
-}
--- src/app/split-google-pay/split-google-pay.component.ts
+++ src/app/split-google-pay/split-google-pay.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SplitPaymentService } from './split-payment.service';
 import { Observable } from 'rxjs';
-import { Recipient } from './recipient.interface';
 
 @Component({
   selector: 'app-split-google-pay',
@@ -9,45 +7,71 @@ import { Recipient } from './recipient.interface';
   styleUrls: ['./split-google-pay.component.css']
 })
 export class SplitGooglePayComponent implements OnInit {
-  totalAmount = 100; // Total payment amount
-  recipients: Recipient[] = [
-    { id: 1, percentage: 50 }, // Recipient 1 gets 50%
-    { id: 2, percentage: 30 }, // Recipient 2 gets 30%
-    { id: 3, percentage: 20 }  // Recipient 3 gets 20%
-  ];
-  splitAmounts: number[] = [];
-  paymentResponse$: Observable<string> | null = null; // Observable to capture the response
+  // Define the percentages for each partner
+  private primaryPercentage = 0.4; // 40%
+  private doctorPercentage = 0.3; // 30%
+  private hospitalPercentage = 0.3; // 30%
 
-  constructor(private splitPaymentService: SplitPaymentService) {}
+  // Total appointments and appointments for each partner
+  private totalAppointments = 0;
+  private primaryAppointments = 0;
+  private doctorAppointments = 0;
+  private hospitalAppointments = 0;
 
-  splitPayment() {
-    const gateway = 'test-gateway'; // Replace with your actual payment gateway
-    const simulateSuccess = false; // Set to true to simulate success for testing
-
-    this.paymentResponse$ = this.splitPaymentService.splitPayment(
-      this.totalAmount,
-      this.recipients,
-      gateway,
-      simulateSuccess
-    );
-
-    // Subscribe to the observable to handle the response
-    this.paymentResponse$.subscribe(
-      (response: string) => {
-        // Handle successful payment response
-        console.log(response);
-        this.splitAmounts = this.recipients.map((recipient, index) => {
-          return (recipient.percentage / 100) * this.totalAmount;
-        });
-      },
-      (error) => {
-        // Handle payment failure here
-        console.error(error);
+  paymentRequest: google.payments.api.PaymentDataRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['MASTERCARD', 'VISA']
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId'
+          }
+        }
       }
-    );
-  }
+    ],
+    merchantInfo: {
+      merchantId: '17613812255336763067',
+      merchantName: 'Demo Only (you will not be charged)'
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPriceLabel: 'Total',
+      totalPrice: '0.00', // Set an initial value, it will be updated later
+      currencyCode: 'USD',
+      countryCode: 'US'
+    }
+  };
+
+  constructor() {}
 
   ngOnInit(): void {
-    console.log("The component is loading");
+    // Calculate the number of appointments for each partner based on percentages
+    this.totalAppointments = this.totalAppointments;
+    this.primaryAppointments = Math.floor(this.totalAppointments * this.primaryPercentage);
+    this.doctorAppointments = Math.floor(this.totalAppointments * this.doctorPercentage);
+    this.hospitalAppointments = Math.floor(this.totalAppointments * this.hospitalPercentage);
+
+    // Calculate the total price based on the number of appointments (you need to define a price per appointment)
+    const pricePerAppointment = 40;
+    this.paymentRequest.transactionInfo.totalPrice = (this.totalAppointments * pricePerAppointment).toFixed(2);
+  }
+
+  async onLoadPaymentData(event: Event) {
+    const paymentData = (event as CustomEvent<google.payments.api.PaymentData>).detail;
+    // Handle payment data here.
+    // You can also update the totalPrice in this method if needed.
+
+    // Example: this.paymentRequest.transactionInfo.totalPrice = updatedTotal.toFixed(2);
+
+    // After handling payment data, you can navigate or perform further actions.
+    // this.router.navigate(['/confirm']);
   }
 }
